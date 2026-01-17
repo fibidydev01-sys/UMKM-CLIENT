57fbe8bcce503bd4a7ec780c78379e51d18392e9
Fibidy-Developer <fibidydev01@gmail.com>
2026-01-17 18:18:28 +0700

    fix: disable eslint img:OG

--- src/app/store/[slug]/opengraph-image.tsx
+++ src/app/store/[slug]/opengraph-image.tsx
@@ -135,6 +135,7 @@ export default async function TenantOgImage({ params }: Props) {
             }}
           >
             {tenant.logo ? (
+              // eslint-disable-next-line @next/next/no-img-element
               <img
                 src={tenant.logo}
                 alt={tenant.name}
